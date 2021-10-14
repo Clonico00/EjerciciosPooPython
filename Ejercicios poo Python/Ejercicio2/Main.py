337ebe3787395484b7929774b79cd91e453360be
Javier Martinez Garcia <79504901+Clonico00@users.noreply.github.com>
2021-10-14 17:33:38 +0200

    Cambios

--- Ejercicios poo Python/Ejercicio2/Main.py
+++ Ejercicios poo Python/Ejercicio2/Main.py
@@ -3,25 +3,25 @@ from Lavadora import Lavadora
 from Television import Television
 
 electrodomestico = ()
-electrodomestico = list(electrodomestico)
+electrod = list(electrodomestico)
 
-electrodomestico.insert(-1, Electrodomestico(consumoEnerguia="A", peso=20))
-electrodomestico.insert(-1, Electrodomestico(consumoEnerguia="A", peso=40))
-electrodomestico.insert(-1, Electrodomestico(consumoEnerguia="A", peso=60))
-electrodomestico.insert(-1, Electrodomestico(consumoEnerguia="A", peso=80))
+electrod.insert(-1, Electrodomestico(consumoEnerguia="A", peso=20))
+electrod.insert(-1, Electrodomestico(consumoEnerguia="A", peso=40))
+electrod.insert(-1, Electrodomestico(consumoEnerguia="A", peso=60))
+electrod.insert(-1, Electrodomestico(consumoEnerguia="A", peso=80))
 
-electrodomestico.insert(-1, Lavadora(consumoEnerguia="E", peso=50, carga=30))
-electrodomestico.insert(-1, Lavadora(consumoEnerguia="A", peso=50, carga=60))
+electrod.insert(-1, Lavadora(consumoEnerguia="E", peso=50, carga=30))
+electrod.insert(-1, Lavadora(consumoEnerguia="A", peso=50, carga=60))
 
-electrodomestico.insert(-1, Television(peso=20, resolucion=40))
-electrodomestico.insert(-1, Television(peso=20, resolucion=80))
+electrod.insert(-1, Television(peso=20, resolucion=40))
+electrod.insert(-1, Television(peso=20, resolucion=80))
 
 television = 0
 lavadora = 0
 electrodomestico = 0
 i = 0
 
-for electroc in electrodomestico:
+for electroc in electrod:
     i += 1
     electroc.precioFinal()
 
